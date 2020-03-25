c44cce716145f0762c568db2a33e6f5826a6c8d5
rafperetz <rafaelperetz2@gmail.com>
2020-03-25 19:08:11 +0200

    added my firt project.

---
+++ FIrst/FIrst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIrst
+{
+    class Program
+    {
+        // Binary tree => max node
+        static void Main(string[] args)
+        {
+        }
+        static int MaxNode()
+    }
+}
